60d7906ecdacf429579be4ce803a66ad5a079835
Aaron Isakov <aaronisakov@gmail.com>
2022-03-15 16:29:12 -0400

    Fixed read_query and made functions for all boolean checks

--- backend/server.py
+++ backend/server.py
@@ -44,8 +44,6 @@ TODO:
 Need to create
 - function for weekly data 
 - function for monthly data
-- fix read_query function
-- 4/4 DONE function for querying and appending ticker table if necessary by querying both ticker and previous queries table
 """
 
 def str_to_date(val : str):
@@ -78,6 +76,14 @@ def request_and_scrub_data(ticker, start, end, conn):
             "end" : end
         }
     )
+    stock_db_conn.execute(
+        """
+        DELETE from {}
+        WHERE ROWID NOT IN (SELECT MIN(ROWID)
+        FROM {}
+        GROUP BY date)
+        """.format(ticker, ticker)
+    )
     print(stock_db_engine.table_names())
     stock_data = stock_data.to_json(orient="index")
     data = json.loads(stock_data)
@@ -96,28 +102,94 @@ def fetch_data_from_db(ticker, start, end):
     stock_data.Date = pd.to_datetime(stock_data.Date)
     stock_data.Date = (stock_data.Date - pd.Timestamp("1970-01-01")) // pd.Timedelta('1s')
     stock_data.set_index('Date', inplace=True)
+    print(stock_data)
     stock_data = stock_data.to_json(orient="index", date_format="epoch")
     data = json.loads(stock_data)
     return data
 
+def start_not_found_request_data(ticker, start, end, conn):
+    print("START NOT FOUND, MADE REQUEST, ADDED TO DB, AND FETCHED FROM DB")
+    stock_data = yf.Ticker(ticker)
+    stock_data = stock_data.history(start=start, end=end, interval=DAILY_CODE)
+    stock_data = stock_data.iloc[:,OPEN_COLUMN:VOLUME_COLUMN]
+    stock_data = stock_data.round(2)
+    stock_data.reset_index(inplace=True)
+    stock_data['Date'] = stock_data['Date'].dt.date
+    stock_data.set_index('Date', inplace=True)
+    stock_data.to_sql(name=ticker, con=conn, if_exists='append')
+    # Insert the ticker + dates so we have a historical timestamp of already known stored data
+    stock_db_conn.execute(
+        prev_queries_insert, 
+        {
+            "ticker" : ticker, 
+            "start" : start, 
+            "end" : str_to_date(end)
+        }
+    )
+    stock_db_conn.execute(
+        """
+        DELETE from {}
+        WHERE ROWID NOT IN (SELECT MIN(ROWID)
+        FROM {}
+        GROUP BY date)
+        """.format(ticker, ticker)
+    )            
+    data = fetch_data_from_db(ticker, start, end)
+    return data
+
+def end_not_found_request_data(ticker, start, end, conn):
+    print("END NOT FOUND, MADE REQUEST, ADDED TO DB, AND FETCHED FROM DB")
+    stock_data = yf.Ticker(ticker)
+    stock_data = stock_data.history(start=start, end=end, interval=DAILY_CODE)
+    stock_data = stock_data.iloc[:,OPEN_COLUMN:VOLUME_COLUMN]
+    stock_data = stock_data.round(2)
+    stock_data.reset_index(inplace=True)
+    stock_data['Date'] = stock_data['Date'].dt.date
+    stock_data.set_index('Date', inplace=True)
+    stock_data.to_sql(name=ticker, con=conn, if_exists='append')
+    # Insert the ticker + dates so we have a historical timestamp of already known stored data
+    stock_db_conn.execute(
+        prev_queries_insert, 
+        {
+            "ticker" : ticker, 
+            "start" : str_to_date(start), 
+            "end" : end
+        }
+    )
+    # delete any duplicate dates we may add since end date will overlap with existing data
+    stock_db_conn.execute(
+        """
+        DELETE from {}
+        WHERE ROWID NOT IN (SELECT MIN(ROWID)
+        FROM {}
+        GROUP BY date)
+        """.format(ticker, ticker)
+    )            
+    data = fetch_data_from_db(ticker, start, end)
+    return data
+
 @app.get("/")
 async def read_query(
-    ticker : str = "NFLX", start : str = "2011-11-01", end : str = "2012-02-01", interval : str = "1d"
+    ticker : str = "TSLA", 
+    start : str = "2012-09-01", 
+    end : str = "2012-12-01", 
+    interval : str = "1d"
     ):
     start = str_to_date(start)
     end = str_to_date(end)
     # Check if the table exists, if it does query DB and make API requests if data missing
     if stock_db_insp.has_table(ticker):
-        # Need reflect in order for DB to be read and not return any errors server crashes without this line of code
+        # Need reflect in order for DB to be read and not return any errors 
+        # server crashes without this line of code
         MetaData.reflect(stock_db_meta)
-        
+        # see if start date already in db
         earliest_start_date = stock_db_conn.execute(
             """
             SELECT ticker, MIN(start), MIN(end) FROM 'Previous Queries'
             WHERE ticker = '{}' AND start <= '{}' AND end >= {};
             """.format(ticker, start, start)
         )
-        
+        # see if end date already in db
         latest_end_date = stock_db_conn.execute(
             """
             SELECT ticker, min(start), MIN(end) FROM 'Previous Queries'
@@ -127,53 +199,24 @@ async def read_query(
         
         earliest_start_date = [row for row in earliest_start_date.first()]
         latest_end_date = [row for row in latest_end_date.first()]
+        print(earliest_start_date)
+        print(latest_end_date)
 
-        if (earliest_start_date == latest_end_date and 
-            earliest_start_date[TICKER_COL] != None and 
-            latest_end_date[TICKER_COL] != None):
-            return fetch_data_from_db(ticker, start, end)
+        if (earliest_start_date[TICKER_COL] != None and latest_end_date[TICKER_COL] != None):
+            data = fetch_data_from_db(ticker, start, end)
+            return data
+            
+        elif earliest_start_date[TICKER_COL] == None and latest_end_date[TICKER_COL] != None:
+            data = start_not_found_request_data(ticker, start, latest_end_date[START_COL], stock_db_engine)
+            return data
+
+        elif earliest_start_date[TICKER_COL] != None and latest_end_date[TICKER_COL] == None:
+            data = end_not_found_request_data(ticker, earliest_start_date[END_COL], end, stock_db_engine)
+            return data
+
         elif earliest_start_date[TICKER_COL] == None and latest_end_date[TICKER_COL] == None:
             print("FETCHED DATA FROM API")
-            return request_and_scrub_data(ticker, start, end, stock_db_engine)
-        elif earliest_start_date[TICKER_COL] == None and latest_end_date[TICKER_COL] != None:
-            print("START NOT FOUND, MADE REQUEST, ADDED TO DB, AND FETCHED FROM DB")
-            stock_data = yf.Ticker(ticker)
-            stock_data = stock_data.history(start=start, end=latest_end_date[START_COL], interval=DAILY_CODE)
-            stock_data = stock_data.iloc[:,OPEN_COLUMN:VOLUME_COLUMN]
-            stock_data = stock_data.round(2)
-            stock_data.reset_index(inplace=True)
-            stock_data['Date'] = stock_data['Date'].dt.date
-            stock_data.set_index('Date', inplace=True)
-            stock_data.to_sql(name=ticker, con=stock_db_engine, if_exists='append')
-            # Insert the ticker + dates so we have a historical timestamp of already known stored data
-            stock_db_conn.execute(
-                prev_queries_insert, 
-                {
-                    "ticker" : ticker, 
-                    "start" : start, 
-                    "end" : end
-                }
-            )
-            return fetch_data_from_db(ticker, start, end)
-        elif earliest_start_date[TICKER_COL] != None and latest_end_date[TICKER_COL] == None:
-            print("END NOT FOUND, MADE REQUEST, ADDED TO DB, AND FETCHED FROM DB")
-            stock_data = yf.Ticker(ticker)
-            stock_data = stock_data.history(start=earliest_start_date[END_COL], end=end, interval=DAILY_CODE)
-            stock_data = stock_data.iloc[:,OPEN_COLUMN:VOLUME_COLUMN]
-            stock_data = stock_data.round(2)
-            stock_data.reset_index(inplace=True)
-            stock_data['Date'] = stock_data['Date'].dt.date
-            stock_data.set_index('Date', inplace=True)
-            stock_data.to_sql(name=ticker, con=stock_db_engine, if_exists='append')
-            # Insert the ticker + dates so we have a historical timestamp of already known stored data
-            stock_db_conn.execute(
-                prev_queries_insert, 
-                {
-                    "ticker" : ticker, 
-                    "start" : start, 
-                    "end" : end
-                }
-            )
-            return fetch_data_from_db(ticker, start, end)
+            data = request_and_scrub_data(ticker, start, end, stock_db_engine)
+            return data
     else:
         return request_and_scrub_data(ticker, start, end, stock_db_engine)
